8f1b89332753d70d7ad504415aac6bbe30bd1cbb
themightykai90 <44848310+themightykai90@users.noreply.github.com>
2018-11-07 20:57:43 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+//
+// Luke's BlackJack Test
+//
+let deck=[
+  "Ace O' Spades",
+  "Two O' Spades",
+  "Three O' Spades"
+  
+  
+  
+  ];
+
+let playerCards=[deck[0],deck[2] ];
+    
+    console.log("Welcome to Luke's BlackJack");
+    
+console.log("You've been dealt: ");
+console.log(" "+playerCards[0]);
+console.log(" "+playerCards[1]);
+console.log(" "+playerCards[2]);
