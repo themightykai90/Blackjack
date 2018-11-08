d7f0905e59b4f33e8b3269ffa7866135f50ba3c2
themightykai90 <44848310+themightykai90@users.noreply.github.com>
2018-11-08 21:27:35 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 //
 // Luke's BlackJack Test
 //
-let deck=[
-  "Ace O' Spades",
-  "Two O' Spades",
-  "Three O' Spades"
+
+
+let suits=['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let values=['Ace','King','Queen', 'Jack',
+'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five',
+'Four', 'Three', 'Two'];
+
+let deck=[];
+
+for (let suitIdx= 0; suitIdx < suits.length; suitIdx++) {
   
-  
-  
-  ];
+  for (let valueIdx= 0; valueIdx < values.length; valueIdx++) {
+  deck.push(values[valueIdx]+ ' of ' + suits[suitIdx]);
+}
+}
+
+for (let i=0; i < deck.length; i++){
+  console.log(deck[i]);
+}
 
 let playerCards=[deck[0],deck[2] ];
     
