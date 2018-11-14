dd36d6a272125630180b2c04a0bf6867199d0e3d
themightykai90 <44848310+themightykai90@users.noreply.github.com>
2018-11-14 21:15:31 +0000

    Update script.js
    Added in some functions to pass and control cards when dealing.

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-//
-// Luke's BlackJack Test
-//
-
-
 let suits=['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values=['Ace','King','Queen', 'Jack',
 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five',
 'Four', 'Three', 'Two'];
 
-let deck=[];
 
+function createDeck(){
+let deck =[];
 for (let suitIdx= 0; suitIdx < suits.length; suitIdx++) {
   
   for (let valueIdx= 0; valueIdx < values.length; valueIdx++) {
@@ -17,11 +13,23 @@ for (let suitIdx= 0; suitIdx < suits.length; suitIdx++) {
 }
 }
 
+  return deck;
+  
+}
+
+function getNextCard() {
+  return deck.shift();
+}
+
+
+let deck= createDeck();
+
+
 for (let i=0; i < deck.length; i++){
   console.log(deck[i]);
 }
 
-let playerCards=[deck[0],deck[2] ];
+let playerCards=[getNextCard(),getNextCard()];
     
     console.log("Welcome to Luke's BlackJack");
     
